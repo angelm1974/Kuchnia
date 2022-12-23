72a648ab4b1e54a8a85cfc05b3f9880b00ef7ab4
angelm1974b <mariusz980@gmail.com>
2022-12-23 15:46:59 +0100

    przedostatni

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,18 @@
 import React from 'react';
 import { View } from 'react-native';
 import {Button, Layout, Text } from '@ui-kitten/components';
+import axios from 'axios';
 export function HomeScreen({ navigation }) {
+ 
+  // const getDataUsingSimpleGetCall = () => {
+  //   // const [dane, setDane] = React.useState(null)
+  //   axios.get('https://kuchnia-c703d-default-rtdb.europe-west1.firebasedatabase.app/Categories.json')
+  //       .then(response =>
+  //         {
+  //          setDane(response.data);
+  //         }
+  //       );
+  // }
   return (
     <Layout style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
@@ -12,6 +23,15 @@ export function HomeScreen({ navigation }) {
         onPress={() => navigation.navigate('Categories')}>
       Przejdź do ekranu szczegółów
     </Button>
+    {/* <Button 
+        appearance='outline' 
+        status='warning'
+        onPress={getDataUsingSimpleGetCall}>
+      dane
+    </Button> */}
+    {/* <View>
+      {dane.id}
+    </View> */}
 
     </Layout>
   );
